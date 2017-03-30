49116bc02e4879e0cc30d1e3053c61c27fea8f0d
Stig Sandbeck Mathisen <ssm@fnord.no>
2017-03-30 13:00:03 +0200

    Document ipsec::install and ipsec::service

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,5 @@
+# This class maintains the required ipsec packages. There are no user
+# servicable parts here. Everything is in the 'ipsec' class.
 class ipsec::install {
   package { 'libreswan':
     ensure => installed,
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,5 @@
+# This class maintains the required ipsec service. There are no user
+# servicable parts here. Everything is in the 'ipsec' class.
 class ipsec::service {
   service { 'ipsec':
     ensure => running,
